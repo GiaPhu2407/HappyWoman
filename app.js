5cd6354a354913a9330490bfa31de1063e492b32
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-10-20 13:36:04 +0700

    20/10

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ button.addEventListener("click", function(){
 function progressInterva(){
     if(count == 100 && percentWidth == 100){
         clearInterval(progressLoad)
-        percent.textContent = "Ok rùi đó:)";
+        percent.textContent = "Vào nhận quà thoi 😉 ";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
